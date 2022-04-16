f35bdee9b04b730ca0e293dd1134ae1b07839b55
minsgy <alstjrchl97@likelion.org>
2022-04-16 18:00:31 +0900

    🎨 style : TAB LIST 상수값 정의 및 쿼리스트링 TAB ID 적용

---
+++ src/constants/queryString.ts
@@ -0,0 +1,2 @@
+export const TAB_ID = 'tab';
+export const SEARCH_ID = 'q';
---
+++ src/constants/tab.ts
@@ -0,0 +1,10 @@
+export const TAB_LIST = [
+  {
+    title: '검색',
+    path: 'search',
+  },
+  {
+    title: '즐겨찾기',
+    path: 'favorite',
+  },
+];
