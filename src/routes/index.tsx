8e5f979b2916bbf998a997cd111fa4297cc17474
minsgy <alstjrchl97@likelion.org>
2022-04-18 02:59:26 +0900

    ✨ feat : Router MainPage 적용

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes as ReactRoutes, Route, Navigate } from 'react-router-dom';
+import { MainPage } from '@pages';
 const Router = () => {
   return (
     <BrowserRouter>
       <ReactRoutes>
-        <Route path="/movies" element={<div>list</div>} />
+        <Route path="/movies" element={<MainPage />} />
         <Route path="*" element={<Navigate to={'/movies'} />} />
       </ReactRoutes>
     </BrowserRouter>
