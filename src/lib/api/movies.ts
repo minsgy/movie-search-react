2c3a2431f82703c070a6f882cc50edafd59e5604
minsgy <alstjrchl97@likelion.org>
2022-04-18 19:24:36 +0900

    🎨 style : Error Message 변경

--- src/lib/api/movies.ts
+++ src/lib/api/movies.ts
@@ -23,7 +23,6 @@ export const fetchGetMovies = async ({ keyword, page }: ISearchMovieRequest) => 
     case 'True': {
       // 영화 검색 조회 성공
       const totalPageCount = getTotalPageCount(Number(data.totalResults), PAGING_SIZE); // 영화 검색 페이지 수
-      console.log(totalPageCount);
       const isLast = page === totalPageCount; // 나올 수 있는 총 페이지 Size와
       return {
         Search: data.Search,
@@ -34,7 +33,7 @@ export const fetchGetMovies = async ({ keyword, page }: ISearchMovieRequest) => 
     }
     case 'False':
     default:
-      // 영화 검색 조회 실패 및 그 외 에러
-      throw new Error('데이터를 처리하지 못했습니다.');
+      // 영화 검색 조회 실패
+      throw new Error('검색 결과가 없습니다.');
   }
 };
